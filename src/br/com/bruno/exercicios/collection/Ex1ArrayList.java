6f0aea63ddcc541bbc5cbf142ce0816d255e050d
Bruno Pires <brunompirescardoso@gmail.com>
2024-01-07 09:12:54 -0300

    feat(Ex1ArrayList.java): Finaliza a classe Finaliza o primeiro exercicio

--- src/br/com/bruno/exercicios/collection/Ex1ArrayList.java
+++ src/br/com/bruno/exercicios/collection/Ex1ArrayList.java
@@ -1,5 +1,34 @@
 package br.com.bruno.exercicios.collection;
 
-public class Ex1ArrayList {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 
+public class Ex1ArrayList {
+    public static void main(String[] args) {
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+        numbers.add(4);
+        numbers.add(5);
+
+        numbers.remove(3);
+
+        int intSize = numbers.size();
+        System.out.println(intSize);
+
+        int specifiedNumber = numbers.get(0);
+        System.out.println(specifiedNumber);
+
+        numbers.add(0, 10);
+
+        Collections.sort(numbers);
+
+
+
+
+
+    }
 }
