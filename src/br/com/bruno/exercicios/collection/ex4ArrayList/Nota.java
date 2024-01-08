fea607c41680a2a8636ac119c6e0ed7148d714fc
Bruno Pires <brunompirescardoso@gmail.com>
2024-01-08 19:10:13 -0300

    feat(ex4ArrayList): Finaliza classe Nota

--- src/br/com/bruno/exercicios/collection/ex4ArrayList/Nota.java
+++ src/br/com/bruno/exercicios/collection/ex4ArrayList/Nota.java
@@ -1,7 +1,13 @@
 package br.com.bruno.exercicios.collection.ex4ArrayList;
 
 public class Nota {
+    private double valor;
 
+    public Nota(int valor){
+        this.valor = valor;
+    }
 
-
+    public double getValor() {
+        return this.valor;
+    }
 }
