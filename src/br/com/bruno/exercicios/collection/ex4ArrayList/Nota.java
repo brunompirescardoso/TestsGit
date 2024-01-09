e135f0f5b87424564f273378dbc26952bf583a23
Bruno Pires <brunompirescardoso@gmail.com>
2024-01-09 19:13:09 -0300

    feat(ex4ArrayList): Finaliza projeto Finaliza as classes com métodos, teste ainda não efetuado

--- src/br/com/bruno/exercicios/collection/ex4ArrayList/Nota.java
+++ src/br/com/bruno/exercicios/collection/ex4ArrayList/Nota.java
@@ -1,13 +1,18 @@
 package br.com.bruno.exercicios.collection.ex4ArrayList;
 
-public class Nota {
+public class Nota implements Comparable<Nota>{
     private double valor;
 
-    public Nota(int valor){
+    public Nota(double valor){
         this.valor = valor;
     }
 
     public double getValor() {
         return this.valor;
+    }
+
+    @Override
+    public int compareTo(Nota outraNota) {
+        return (int) (this.getValor() - outraNota.getValor());
     }
 }
